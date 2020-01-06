34036dad2d6c9d0aa2189282536405a2d8bd4652
dzezo <dzezonikola@gmail.com>
2020-01-06 04:39:39 +0100

    heroku

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
 	database: 'mongodb+srv://nikola:@lozinka123@cluster0-xienn.mongodb.net/test?retryWrites=true&w=majority',
 	secret: 'B13C46247F10B2D242D22A393EE2AB2FF06CD5AAF1A8D687B110FAB6C50618FF'
-	
 }
