3e00f5d723b12d88778a0dbc776478c0374f6bae
dzezo <dzezonikola@gmail.com>
2020-01-06 04:36:39 +0100

    m

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
 	database: 'mongodb+srv://nikola:@lozinka123@cluster0-xienn.mongodb.net/test?retryWrites=true&w=majority',
 	secret: 'B13C46247F10B2D242D22A393EE2AB2FF06CD5AAF1A8D687B110FAB6C50618FF'
+	
 }
