28000d85b6719dd1546e781c513c290a4ebcf444
dzezo <dzezonikola@gmail.com>
2020-01-06 04:32:09 +0100

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	database: 'mongodb://localhost:27017/yourgym',
+	database: 'mongodb+srv://nikola:@lozinka123@cluster0-xienn.mongodb.net/test?retryWrites=true&w=majority',
 	secret: 'B13C46247F10B2D242D22A393EE2AB2FF06CD5AAF1A8D687B110FAB6C50618FF'
 }
