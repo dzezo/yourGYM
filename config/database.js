78c8af87db644fedcc110eb08e91e50efaafb068
dzezo <dzezonikola@gmail.com>
2020-01-07 04:44:48 +0100

    changed node driver

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	database: 'mongodb+srv://nikola:@lozinka123@cluster0-xienn.mongodb.net/test?retryWrites=true&w=majority',
+	database: 'mongodb://nikola1234:nikola1234@cluster0-shard-00-00-xienn.mongodb.net:27017,cluster0-shard-00-01-xienn.mongodb.net:27017,cluster0-shard-00-02-xienn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
 	secret: 'B13C46247F10B2D242D22A393EE2AB2FF06CD5AAF1A8D687B110FAB6C50618FF'
 }
